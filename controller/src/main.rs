94243b90d69c374f8702b5c4c4db3176d667a623
Martin Hughes <martin-hughes@users.noreply.github.com>
2025-01-14 09:19:01 +0000

    Poke thermometer to see if temperatures come back
    They do not.

---
+++ controller/src/main.rs
@@ -0,0 +1,69 @@
+mod thermopro;
+
+use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter};
+use btleplug::platform::Manager;
+use std::error::Error;
+use std::time::Duration;
+use tokio::time;
+use crate::thermopro::{is_relevant, run_for_device};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+
+    let manager = Manager::new().await?;
+    let adapter_list = manager.adapters().await?;
+    if adapter_list.is_empty() {
+        eprintln!("No Bluetooth adapters found");
+    }
+
+    for adapter in adapter_list.iter() {
+        println!("Starting scan...");
+        adapter
+            .start_scan(ScanFilter::default())
+            .await
+            .expect("Can't scan BLE adapter for connected devices...");
+        time::sleep(Duration::from_secs(2)).await;
+        let peripherals = adapter.peripherals().await?;
+
+        if peripherals.is_empty() {
+            eprintln!("->>> BLE peripheral devices were not found, sorry. Exiting...");
+        } else {
+            // All peripheral devices in range.
+            for peripheral in peripherals.iter() {
+                let properties = peripheral.properties().await?;
+                let is_connected = peripheral.is_connected().await?;
+                let local_name = properties
+                    .unwrap()
+                    .local_name
+                    .unwrap_or(String::from("(peripheral name unknown)"));
+                println!(
+                    "Peripheral {:?} is connected: {:?}",
+                    &local_name, is_connected
+                );
+                // Check if it's the peripheral we want.
+                if is_relevant(local_name.as_str()) {
+                    println!("Found matching peripheral {:?}...", &local_name);
+                    if !is_connected {
+                        // Connect if we aren't already connected.
+                        if let Err(err) = peripheral.connect().await {
+                            eprintln!("Error connecting to peripheral, skipping: {}", err);
+                            continue;
+                        }
+                    }
+                    let is_connected = peripheral.is_connected().await?;
+                    println!(
+                        "Now connected ({:?}) to peripheral {:?}.",
+                        is_connected, &local_name
+                    );
+                    if is_connected {
+                        let _ = run_for_device(peripheral).await;
+                    }
+                } else {
+                    println!("Skipping unknown peripheral {:?}", peripheral);
+                }
+            }
+        }
+    }
+    Ok(())
+}
---
+++ controller/src/thermopro.rs
@@ -0,0 +1,75 @@
+use btleplug::api::{CharPropFlags, Characteristic, Peripheral, WriteType};
+use futures::stream::StreamExt;
+use std::error::Error;
+use std::time::Duration;
+use tokio::time::timeout;
+use uuid::Uuid;
+
+const THERMOPRO_NAME: &str = "Thermopro";
+
+/// UUID of the characteristic for which we should subscribe to notifications.
+const WRITE_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x1086fff1_3343_4817_8bb2_b32206336ce8);
+const NOTIFY_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x1086fff2_3343_4817_8bb2_b32206336ce8);
+
+pub fn is_relevant(name: &str) -> bool {
+    name == THERMOPRO_NAME
+}
+
+fn is_notify_characteristic(characteristic: &Characteristic) -> bool {
+    characteristic.uuid == NOTIFY_CHARACTERISTIC_UUID
+        && characteristic.properties.contains(CharPropFlags::NOTIFY)
+}
+
+fn is_write_characteristic(characteristic: &Characteristic) -> bool {
+    characteristic.uuid == WRITE_CHARACTERISTIC_UUID
+        && characteristic.properties.contains(CharPropFlags::WRITE)
+}
+
+pub async fn run_for_device<P: Peripheral>(device: &P) -> Result<(), Box<dyn Error>> {
+    println!("Discover peripheral services...");
+    device.discover_services().await?;
+    let characteristics = device.characteristics();
+    let notify_characteristic = characteristics
+        .iter()
+        .find(|c| c.uuid == NOTIFY_CHARACTERISTIC_UUID)
+        .unwrap();
+    let write_characteristic = characteristics
+        .iter()
+        .find(|c| c.uuid == WRITE_CHARACTERISTIC_UUID)
+        .unwrap();
+
+    println!("Checking characteristic {:?}", notify_characteristic);
+    // Subscribe to notifications from the characteristic with the selected
+    // UUID.
+    if !is_notify_characteristic(notify_characteristic)
+        || !is_write_characteristic(write_characteristic)
+    {
+        return Err("Bad characteristics".into());
+    }
+
+    println!(
+        "Subscribing to characteristic {:?}",
+        notify_characteristic.uuid
+    );
+    device.subscribe(&notify_characteristic).await?;
+    const CMD: [u8; 3] = [0x30, 0, 0x30];
+
+    device
+        .write(write_characteristic, &CMD, WriteType::WithoutResponse)
+        .await?;
+
+    // Print the first 4 notifications received.
+    let mut notification_stream = device.notifications().await?.take(4);
+    // Process while the BLE connection is not broken or stopped.
+    while let Some(data) = timeout(Duration::from_secs(4), notification_stream.next())
+        .await
+        .unwrap()
+    {
+        println!("Received data from [{:?}]: {:?}", data.uuid, data.value);
+    }
+
+    println!("Disconnecting from peripheral...");
+    device.disconnect().await.unwrap();
+
+    Ok(())
+}
